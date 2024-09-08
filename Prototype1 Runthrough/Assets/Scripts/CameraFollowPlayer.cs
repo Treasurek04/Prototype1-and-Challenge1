993526c9b428670c5b3a3b3b5ef621396675a59d
Treasurek04 <treasurekeys2004@gmail.com>
2024-09-07 19:22:29 -0500

    Created CamFollowPlayer script and PlayerController script

---
+++ Prototype1 Runthrough/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿/*
+ Treasure Keys 
+Prototype 1
+Camera Follows Player Setup
+ 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+
+    private Vector3 offset = new Vector3(0, 5, -10);
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
