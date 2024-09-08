2db00dc05cc76351bcce413b4b844cc45bf22e57
Treasurek04 <treasurekeys2004@gmail.com>
2024-09-07 23:09:44 -0500

    Created Text Box and ScoreManager Script

---
+++ Prototype1 Runthrough/Assets/Scripts/PlayerLoseOnFall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Add this using statement when using Text UI elements
+using UnityEngine.UI;
+
+public class PlayerLoseOnFall : MonoBehaviour
+{
+
+    public Text textbox;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -1)
+        {
+            textbox.text = "You Lose!";
+        }
+    }
+}
---
+++ Prototype1 Runthrough/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static bool gameOver = false;
+    public static bool won = false;
+    public static int score = 0;
+
+    public Text textbox;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
