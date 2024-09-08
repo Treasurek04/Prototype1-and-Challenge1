4ba22eda4f5d55c8f356d879d9867d45370a2abd
Treasurek04 <treasurekeys2004@gmail.com>
2024-09-08 01:09:07 -0500

    Created new Script
    Created a script for color change on collision

---
+++ Prototype1 Runthrough/Assets/Scripts/ColorChangeOnCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeOnCollision : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Turn this object Red
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+   
+}
