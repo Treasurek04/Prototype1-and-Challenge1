3edf8072b1e5f8b91298070690acb0f1807e6526
Treasurek04 <treasurekeys2004@gmail.com>
2024-09-07 20:29:44 -0500

    Created YouWin Textbox Trigger Zone

---
+++ Prototype1 Runthrough/Assets/Scripts/PlayerEnterTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Add this using statement when using Text UI elements 
+using UnityEngine.UI;
+
+public class PlayerEnterTrigger : MonoBehaviour
+{
+    public Text textbox;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Finish"))
+        {
+            //set the textbox text to "You Win!
+            textbox.text = "You Win!";
+        }
+            
+    }
+   
+}
