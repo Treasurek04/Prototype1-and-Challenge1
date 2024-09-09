23d8564d116376bae491858371d93a738cec7973
Treasurek04 <treasurekeys2004@gmail.com>
2024-09-08 22:10:35 -0500

    Created TriggerZone Adds to Score script

---
+++ Challenge1 Runthrough/Assets/Challenge 1/Scripts/TriggerZoneAddsScore.cs
@@ -0,0 +1,25 @@
+﻿/*
+ Treasure Keys 
+Challenge 1
+When Player enters the trigger zone then the score gets incremanted once 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZoneAddsScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool triggered = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            ScoreManager.score++;
+        }
+    }
+}
