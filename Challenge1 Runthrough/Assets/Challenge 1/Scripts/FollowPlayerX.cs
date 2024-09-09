3c42ededcbcbefcaa31e40ea8390841a97536d0c
Treasurek04 <treasurekeys2004@gmail.com>
2024-09-08 19:54:09 -0500

    Updated Challenge1 scripts
    Edited FollowPlane and PlaneMovement scripts

---
+++ Challenge1 Runthrough/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,28 @@
+﻿/*
+Treasure Keys 
+Challenge 1
+Correcting Camera to be behind the Player
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+     offset = new Vector3(0, 5, -10);
+
+}
+
+// Update is called once per frame
+void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
