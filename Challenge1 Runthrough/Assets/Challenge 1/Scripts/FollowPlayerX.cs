7755b443166970cb811993755e7e8f4ff7abe29a
Treasurek04 <treasurekeys2004@gmail.com>
2024-09-08 21:34:08 -0500

    Edited Player Controller Script
    Made edits to the Player Controller Script

--- Challenge1 Runthrough/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Challenge1 Runthrough/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -10,18 +10,11 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
-
-    // Start is called before the first frame update
-    void Start()
-    {
+    private Vector3 offset = new Vector3(0, 5, -10);
     
-     offset = new Vector3(0, 5, -10);
-
-}
 
 // Update is called once per frame
-void Update()
+    void Update()
     {
         transform.position = plane.transform.position + offset;
     }
