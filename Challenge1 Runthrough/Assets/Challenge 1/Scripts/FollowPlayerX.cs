38442b1a34fd1f47bab8e1adfb3b60ce7c037607
Treasurek04 <treasurekeys2004@gmail.com>
2024-09-18 22:39:29 -0500

    Update

--- Challenge1 Runthrough/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Challenge1 Runthrough/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3(0, 5, -10);
+    private Vector3 offset = new Vector3(30, 0, 10);
     
 
 // Update is called once per frame
